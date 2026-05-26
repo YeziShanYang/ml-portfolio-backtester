de5a7b6e574befb277f250e35a2a90e82168e2a2
Jason Shi <trapezoidlearning@gmail.com>
2026-05-25 23:01:05 -0500

    refactor: convert project to installable package with setup.py and simulations folder

--- fetch_data.py
+++ fetch_data.py
@@ -3,6 +3,7 @@ import yfinance as yf
 def get_stock_data(ticker):
     print(f"Fetching historical data for {ticker}...")
     
+    # We use yf.Ticker instead of yf.download because we might want to get other info later
     stock = yf.Ticker(ticker)
     data = stock.history(period="60d", interval="1d")
     
