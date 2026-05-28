9cb9169009c310584a3fef43dd28186b3cad42d1
Jason Shi <trapezoidlearning@gmail.com>
2026-05-27 20:30:19 -0500

    feat: add indicator to check for RSI for various stocks

--- src/indicators.py
+++ src/indicators.py
@@ -17,3 +17,26 @@ def detect_golden_cross(sma_fast, sma_slow):
     tickers = has_cross[has_cross == True].index.tolist()
     
     return tickers
+
+def calculate_rsi(df_close, window=14):
+    """
+    RSI is basically an index of how accurate the current pricing is
+    High RSI (>70) = overvalued, a lot more gains than losses (ppl buying)
+    Low RSI (<30) = undervalued, a lot more losses (ppl selling)
+    """
+
+    # Daily difference in prices
+    delta = df_close.diff()
+
+    gains = delta.clip(lower=0)
+    losses = -delta.clip(upper=0)
+
+    # Calculate EMA (exponential moving avg)
+    avg_gain = gains.ewm(com=window-1, adjust=False).mean()
+    avg_loss = losses.ewm(com=window-1, adjust=False).mean()
+
+    rs = avg_gain/avg_loss
+
+    rsi = 100 - (100 / (1 + rs))
+
+    return rsi
