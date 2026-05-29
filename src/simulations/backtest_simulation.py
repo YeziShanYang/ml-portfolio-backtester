96e5c8321095982006be56bd15ec55838b005917
Jason Shi <trapezoidlearning@gmail.com>
2026-05-29 17:46:52 -0500

    refactor: modify backtest_simulation to be more object-oriented, now easier to test data

--- src/simulations/backtest_simulation.py
+++ src/simulations/backtest_simulation.py
@@ -1,21 +1,154 @@
 import pandas as pd
 import yfinance as yf
 from src import stock_screener, indicators
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn import ensemble, linear_model, metrics
 
-"""
-Backtesting Simulation
-Strategy: 
-  - We will use data from a specific stock and see our returns
-  - Our ML models (Linear SVM & Random Forest) will tell us whether to invest or not
-  - We will first implement Random Forst as it performed better in 03_model_training
-ConclusionL
-  - Our simulation tested AAPL with training data MSFT, GOOGL, NVDA. 
-  - We experienced +38.42% returns over 1 year.
-Potential next step is to expand to pool of multiple stocks
-"""
+class BacktestEngine:
+    def __init__(self, model, feature_configs, target_shift=-7):
+        """
+        Here, 'model' is the kind of scikit-learn model we want to use.
+        Since different types of models work differently, we would like to encompass
+        both 0/1 classifiers as well as regression models.
+        'feature_configs' is our dictionary of features we want the model to consider.
+        Each entry is formatted 'feature_name': {'func': function_name, 'params': param_dict}.
+        So for example if we wanted sma_5: 'sma_5':  {'func': indicators.calculate_sma, 'params': {'window': 5}}
+        Note: target_shift must be negative because of the way we shift data for the labels for training
+        """
+        self.model = model
+        self.feature_configs = feature_configs
+        self.target_shift = target_shift
+        self.is_regressor = "Classifier" not in type(model).__name__
+    
+    def build_features(self, close_df):
+        """
+        This function basically allows us to get a dictionary of Pandas Dataframes with the info we need
+        and handles all of the interactions with indicators.py so we don't have to worry about it later.
+        """
+        features_dict = {}
+        for feat, config in self.feature_configs.items():
+            features_dict[feat] = config['func'](close_df, **config['params'])
+        return features_dict
+    
+    def get_training_data(self, training_tickers, close_prices, features, target_labels):
+        """
+        This function is almost a copy-paste of what I wrote for run_backtest_simulation.py
+        in the first version (when everything was still just one function).
+        It takes in all of the info we want and returns the correctly formatted features and label.
+        """
+        training_list = []
+        for ticker in training_tickers:
+            ticker_data = {}
 
+            for feat in self.feature_configs.keys():
+                ticker_data[feat] = features[feat][ticker]
+            
+            ticker_data['target'] = target_labels[ticker]
+            training_stock_df = pd.DataFrame(ticker_data)
+            training_stock_df['ticker'] = ticker
+            training_list.append(training_stock_df)
+        
+        training_df = pd.concat(training_list).dropna()
+        X = training_df.drop(columns=['target', 'ticker'])
+        y = training_df['target']
+        return X, y
+    
+    def run_simulation(self, target_ticker, training_tickers, period="1y", interval="1d"):
+        """
+        This function actually runs our simulation. Thanks to the class/OOP structure we have now,
+        it just requires us to provide its target ticker and training tickers, assuming the
+        features are already loaded.
+        """
+        # First, we get the data for training
+        pandas_df = stock_screener.fetch_screener_data(training_tickers, period=period, interval=interval)
+        close_prices = pandas_df['Close']
+
+        # Thanks to our previous functions, we can build our training models really easily:
+        features = self.build_features(close_prices)
+        future_price = close_prices.shift(self.target_shift)
+        if self.is_regressor:
+            target_labels = (future_price - close_prices) / close_prices
+        else:
+            target_labels = (future_price > close_prices).astype(int)
+
+        X, y = self.get_training_data(training_tickers, close_prices, features, target_labels)
+        self.model.fit(X, y)
+
+        # Now we get the data that we'll be testing it against
+        ticker_df = stock_screener.fetch_screener_data(target_ticker, period=period, interval=interval)
+        ticker_close_prices = ticker_df['Close']
+
+        target_features = self.build_features(ticker_close_prices)
+
+        # We need to flatten this list, because yfinance returns dataframes with a multi-index structure.
+        # We could probably solve this by building another function in stock_screener, but that's a problem for another day.
+        target_stock_dict = {}
+        for feat_name in self.feature_configs.keys():
+            target_stock_dict[feat_name] = target_features[feat_name][target_ticker]
+        target_stock_df = pd.DataFrame(target_stock_dict).dropna()
+
+        # We can now generate model predictions & flatten our data, similar to what we did with the regular backtest function
+        raw_predictions = self.model.predict(target_stock_df)
+        if self.is_regressor:
+            predictions = (raw_predictions > 0).astype(int)
+        else:
+            predictions = raw_predictions
+        test_close_prices = ticker_close_prices[target_ticker].loc[target_stock_df.index].squeeze()
+
+        # Now we use a loop to actually run the simulation
+        starting_capital = 10000.0
+        capital = starting_capital
+        shares_owned = 0
+        stock_owned = False
+
+        for i in range(len(test_close_prices)):
+            current_price = test_close_prices.iloc[i]
+            prediction = predictions[i]
+
+            if prediction == 1 and not stock_owned:
+                shares_owned = capital / current_price
+                capital = 0
+                stock_owned = True
+            elif prediction == 0 and stock_owned:
+                capital = shares_owned * current_price
+                shares_owned = 0
+                stock_owned = False
+        
+        # Now we can evaluate how well our simulation did:
+        final_price = test_close_prices.iloc[-1]
+        final_balance = capital if not stock_owned else (shares_owned * final_price)
+        total_return = ((final_balance - starting_capital) / starting_capital) * 100
+
+        # We calculate the returns for buy-and-hold so we can compare & calculate alpha
+        bh_shares = starting_capital / test_close_prices.iloc[0]
+        bh_final_balance = bh_shares * final_price
+        bh_return = ((bh_final_balance - starting_capital) / starting_capital) * 100
+
+        print(f"\nResults of simulation for {target_ticker} based on {training_tickers} ({type(self.model).__name__})")
+        print(f"Features Utilized       : {list(self.feature_configs.keys())}")
+        print(f"Ending Model Capital    : ${final_balance:,.2f} ({total_return:+.2f}%)")
+        print(f"Baseline Buy & Hold     : ${bh_final_balance:,.2f} ({bh_return:+.2f}%)")
+
+if __name__ == "__main__":
+    # setup feature dictionary
+    my_features = {
+        'sma_5':  {'func': indicators.calculate_sma, 'params': {'window': 5}},
+        'sma_20': {'func': indicators.calculate_sma, 'params': {'window': 20}},
+        'rsi':    {'func': indicators.calculate_rsi, 'params': {'window': 14}}
+    }
+    
+    train_pool = ["MSFT", "GOOGL", "NVDA"]
+    target = "AAPL"
+
+    rf_classifier = ensemble.RandomForestClassifier(n_estimators=100, random_state=42, min_samples_split=10)
+    engine_rf = BacktestEngine(model=rf_classifier, feature_configs=my_features)
+    engine_rf.run_simulation(target_ticker=target, training_tickers=train_pool, period="1y")
+
+    lin_regressor = linear_model.LinearRegression()
+    engine_lr = BacktestEngine(model=lin_regressor, feature_configs=my_features)
+    engine_lr.run_simulation(target_ticker=target, training_tickers=train_pool, period="1y")
+
+
+# I'll leave this here as it's still interesting. May move to a different file later on.
 def run_backtest_simulation(ticker, training_tickers, period="60d", interval="1d"):
     # Our first step is to get the data for training.
     # Here, we will use training_tickers in order to guess ticker.
@@ -57,7 +190,7 @@ def run_backtest_simulation(ticker, training_tickers, period="60d", interval="1d
     y = training_df['target']
 
     # Setup random forest model
-    rf_model = RandomForestClassifier(n_estimators=100, random_state=42, min_samples_split=10)
+    rf_model = ensemble.RandomForestClassifier(n_estimators=100, random_state=42, min_samples_split=10)
     rf_model.fit(X, y)
 
     # Our second step is to setup the data that we want to run the model on (ticker)
@@ -114,11 +247,3 @@ def run_backtest_simulation(ticker, training_tickers, period="60d", interval="1d
     print("\nSimulation Results:")
     print(f"Starting Capital:   ${starting_capital:,.2f}")
     print(f"Ending   Capital:   ${final_balance:,.2f} ({total_return:+.2f}%)")
-
-if __name__ == "__main__":
-    run_backtest_simulation(
-        ticker="AAPL", 
-        training_tickers=["MSFT", "GOOGL", "NVDA"], 
-        period="1y",
-        interval="1d"
-    )
